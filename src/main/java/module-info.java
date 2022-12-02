338e3f954158f29893987393b7384771d889cf31
Daniel Vitor <ripsea.rpg321@gmail.com>
2022-12-02 01:41:36 -0300

    Add dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,14 +9,15 @@ module com.br.confeitarialegal {
   requires javax.json;
   requires java.sql;
   requires java.base;
-  requires org.eclipse.persistence.jpa;
-  requires org.eclipse.persistence.core;
-  requires org.eclipse.persistence.jpa.modelgen.processor;
+//  requires org.eclipse.persistence.jpa;
+//  requires org.eclipse.persistence.core;
+//  requires org.eclipse.persistence.jpa.modelgen.processor;
+  requires de.jensd.fx.glyphs.fontawesome;
 
   opens com.br.confeitarialegal to javafx.fxml;
   opens com.br.confeitarialegal.view to javafx.fxml;
   opens com.br.confeitarialegal.entity;
-  
+
   exports com.br.confeitarialegal;
   exports com.br.confeitarialegal.view;
 }
