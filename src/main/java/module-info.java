42f705b6f64ea8342dda0f42bd53234e5175ea16
Daniel Vitor <ripsea.rpg321@gmail.com>
2022-12-02 03:15:32 -0300

    Remove unnecessary dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,12 +6,8 @@ module com.br.confeitarialegal {
   requires org.hibernate.orm.core;
   requires java.naming;
   requires static lombok;
-  requires javax.json;
   requires java.sql;
   requires java.base;
-//  requires org.eclipse.persistence.jpa;
-//  requires org.eclipse.persistence.core;
-//  requires org.eclipse.persistence.jpa.modelgen.processor;
   requires de.jensd.fx.glyphs.fontawesome;
 
   opens com.br.confeitarialegal to javafx.fxml;
