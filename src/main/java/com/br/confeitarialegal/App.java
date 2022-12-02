4d47b7cacf5cfda283453dc943119462dc4d9cbf
Daniel Vitor <ripsea.rpg321@gmail.com>
2022-12-01 21:49:59 -0300

    Create view of Registration

--- src/main/java/com/br/confeitarialegal/App.java
+++ src/main/java/com/br/confeitarialegal/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-    scene = new Scene(loadFXML("view/primary"), 640, 480);
+    scene = new Scene(loadFXML("view/primary"), 1366, 768);
     stage.setScene(scene);
     stage.show();
   }
---
+++ src/main/java/com/br/confeitarialegal/view/RegistrationController.java
@@ -0,0 +1,22 @@
+package com.br.confeitarialegal.view;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Daniel Vitor
+ */
+public class RegistrationController implements Initializable {
+
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+  }  
+  
+}
